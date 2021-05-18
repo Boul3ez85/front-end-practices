0cccf3954c8ba21dc98a8e111dfa368b1de48654
Boul3ez85 <benrebah.moez@gmail.com>
2021-05-18 08:26:39 +0100

    update navigation to top button behavior

--- customise product form/index.js
+++ customise product form/index.js
@@ -257,15 +257,28 @@ function rem() {
 }
 
 
-//Go to top button:
-var scrollToTopBtn = document.getElementById("scrollToTopBtn");
+// scroll to top button behavior
+var scrollToTopBtn = document.querySelector(".scrollToTopBtn");
 var rootElement = document.documentElement;
 
+function handleScroll() {
+  // Do something on scroll
+  var scrollTotal = rootElement.scrollHeight - rootElement.clientHeight;
+  if ((rootElement.scrollTop / scrollTotal ) > 0.30) {
+    // Show button
+    scrollToTopBtn.classList.add("showBtn");
+  } else {
+    // Hide button
+    scrollToTopBtn.classList.remove("showBtn");
+  }
+}
+
 function scrollToTop() {
+  // Scroll to top logic
   rootElement.scrollTo({
     top: 0,
     behavior: "smooth"
   })
 }
-
-scrollToTopBtn.addEventListener("click", scrollToTop)
+scrollToTopBtn.addEventListener("click", scrollToTop);
+document.addEventListener("scroll", handleScroll);
